98ab2b17dfa3b48740641efd9fc829e45a1405a7
lawrence <lawrenceli@augmentum.com.cn>
2017-04-11 20:10:17 +0800

    lawrence UI

--- ROTK.VoiceAssistant.UI/MainWindow.xaml.cs
+++ ROTK.VoiceAssistant.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using ROTK.VoiceAssistant.UI.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,9 +23,30 @@ namespace ROTK.VoiceAssistant.UI
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        MessageView messagingView ;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+         
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Back.Visibility = Visibility.Collapsed;
+            this.SpecificWorkArea.Children.Clear();
+            this.TileArea.Visibility = Visibility.Visible;
+            DoubleAnimation widthAnimation = new DoubleAnimation(0.0, 1.0, new Duration(TimeSpan.FromSeconds(0.5)));
+            this.TileArea.BeginAnimation(ScrollViewer.OpacityProperty, widthAnimation);
+        }
+
+        private void MessageTileClick(object sender, RoutedEventArgs e)
+        {
+            this.Back.Visibility = Visibility.Visible;
+            this.TileArea.Visibility = Visibility.Collapsed;
+            messagingView = new MessageView();
+            DoubleAnimation widthAnimation = new DoubleAnimation(0.0, 1.0, new Duration(TimeSpan.FromSeconds(0.5)));
+            this.messagingView.BeginAnimation(MessageView.OpacityProperty, widthAnimation);
+            this.SpecificWorkArea.Children.Add(messagingView);
         }
     }
 }
