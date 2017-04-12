2a40343c19a008ae4d51f79a2b07dc909d3105d4
AnselYang <anselyang@augmentum.com.cn>
2017-04-12 22:57:08 +0800

    Check in the messageSentEvent.

---
+++ ROTK.VoiceAssistant.Events/MessageSentEvent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CognitiveServices.SpeechRecognition;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROTK.VoiceAssistant.Events
+{
+   
+    public class MessageSentEvent : PubSubEvent
+    {
+
+    }
+
+}
